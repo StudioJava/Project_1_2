b86619914b0bf17b1bf1549be45796b214b1a29c
StudioJava <38365682+StudioJava@users.noreply.github.com>
2018-11-29 11:24:05 -0400

    update on scrolling function

--- js/main_1_2.js
+++ js/main_1_2.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+window.onscroll = function() {scrollColorChange()};
+
+function scrollColorChange() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("myP").className = "orange";
+    } else {
+        document.getElementById("myP").className = "text-white";
+    }
+}
