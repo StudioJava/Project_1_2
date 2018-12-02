a2e6c92de3df92f53db6384614f87711a333b97b
StudioJava <38365682+StudioJava@users.noreply.github.com>
2018-12-02 08:59:06 -0400

    Finalized update on the javascript code to create the ordered list on the div.

--- js/main_1_2.js
+++ js/main_1_2.js
@@ -1,10 +1,33 @@
 // JavaScript Document
-window.onscroll = function() {scrollColorChange()};
-
-function scrollColorChange() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("myP").className = "orange";
-    } else {
-        document.getElementById("myP").className = "text-white";
-    }
+var person = [];
+//++++++++
+function myFunction(){
+	var firstName = document.getElementById("input-1").value;
+	var lastName = document.getElementById("input-2").value;
+	document.getElementById("demo").innerHTML = firstName + " " + lastName;
+	
+	addName(firstName,lastName);
+	console.log(person);
+	clear();
+}
+//+++++++++
+function addName(first,last){
+	person.push(first +" " + last);
+}
+//++++++++++
+function clear(){
+	document.getElementById("input-1").value = "";
+	document.getElementById("input-2").value = "";
+}
+//++++++
+function print(){
+	
+	var listHTML = "";	
+	var i;
+	for (i = 0; i < person.length; i++) {
+    listHTML += "<li>" +  person[i] + "</li>";
+	};
+	document.getElementById("demo").innerHTML = "Your List of Friends:";
+	document.getElementById("demo2").innerHTML = listHTML;
+	
 }
